c4b158ae93ea3335bfc29d79dadce9038b023fc9
cys <cys@iquest.kr>
2026-02-23 10:30:22 +0900

    feat: add HIGPractice learning app scaffold

---
+++ practice/HIGPractice/HIGPractice/HIGPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIGPracticeApp.swift
+//  HIGPractice
+//
+//  Created by YuSeongChoi on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct HIGPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
